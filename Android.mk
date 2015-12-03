0013adf8280a7f1df4c0077fae4a64549549c1d1
iBullRay <bullray097@yandex.ru>
2015-12-03 22:49:37 +0200

    light: Fixed variable
    Signed-off-by: iBullRay <bullray097@yandex.ru>

--- Android.mk
+++ Android.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # Light driver for ATM702X
-ifeq ($(strip $(BOARD_USES_ACTIONS_LIGHT)), true)
+ifeq ($(BOARD_HAVE_ACTIONS_LIGHT), true)
 
 LOCAL_PATH := $(call my-dir)
 
